a0ad53e38ec356af7c608756c79b1beb8680e921
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2022-11-12 09:48:05 +1100

    - GET routes to display frontend for index '/' - GET routes to display frontend for new '/new' - Implement 'create' action via '/new' POST route - Eval execution sink allows for string escape

--- complex/app.js
+++ complex/app.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const { v4: uuid } = require('uuid')
 
-const cutleries = require('./public/data/data.json').data
+let cutleries = require('./public/data/data.json')
 
-//app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+// https://stackoverflow.com/a/47232318 (about body-parser)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_, res) => {
   res.render('index', { cutleries })
+})
+
+app.get('/new', (_, res) => {
+  res.render('pages/create')
+})
+
+app.post('/new', (req, res) => {
+  let name, price;
+  if (req.body.name && req.body.price) {
+    try {
+      eval('name = "' + req.body.name + '"')
+      eval('price = ' + req.body.price)
+    } catch (err) {
+      name = err.message
+      price = "N/A"
+    }
+  }
+  cutleries.data.push({ "id": uuid(), "name": name, "price": price })
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
